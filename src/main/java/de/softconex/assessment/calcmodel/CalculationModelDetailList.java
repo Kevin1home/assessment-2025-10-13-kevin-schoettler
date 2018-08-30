2e5fd7c5796b83cdc03c44975382fb5ec71bb114
hhaag <Holger.Haag@SoftConEx.de>
2018-08-30 14:56:56 +0200

    Removed unused SuppressWarnings

--- src/main/java/de/softconex/assessment/calcmodel/CalculationModelDetailList.java
+++ src/main/java/de/softconex/assessment/calcmodel/CalculationModelDetailList.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 /**
  * List of {@link CalculationModelDetail} objects.
  */
-@SuppressWarnings("serial")
 public class CalculationModelDetailList extends ArrayList<CalculationModelDetail> {
 	public CalculationModelDetail find(final Price price) {
 		return this.stream()
